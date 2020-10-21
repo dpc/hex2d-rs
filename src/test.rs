fb64a547200cf02725860222f0c8362bef5546cc
Ricardo Ardissone <ricardo.ardissone@gmail.com>
2020-10-21 17:20:53 -0300

    split the 3 line_to test fns

--- src/test.rs
+++ src/test.rs
@@ -359,7 +359,19 @@ fn simple_line_to() {
 fn line_to_iter() {
     with_pair_test_points(|a: Coordinate, b: Coordinate| {
         assert_eq!(a.line_to(b), a.line_to_iter(b).collect::<Vec<_>>());
+    });
+}
+
+#[test]
+fn line_to_lossy_iter() {
+    with_pair_test_points(|a: Coordinate, b: Coordinate| {
         assert_eq!(a.line_to_lossy(b), a.line_to_lossy_iter(b).collect::<Vec<_>>());
+    });
+}
+
+#[test]
+fn line_to_with_edge_detection_iter() {
+    with_pair_test_points(|a: Coordinate, b: Coordinate| {
         assert_eq!(a.line_to_with_edge_detection(b), a.line_to_with_edge_detection_iter(b).collect::<Vec<_>>());
     });
 }
